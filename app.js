a4821d4e2cfe163508597978f3962587cbd72c30
simonasstaskevicius <simonas.staskevicius@hostinger.com>
2025-04-15 22:47:29 +0300

    auth

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const express = require('express');
-
+const authRoute = require('./routes/authRoute');
 
 const app = express();
+
+app.use(express.json());
+
+app.use('/api/v1/auth', authRoute);
+
+module.exports = app;
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,14 +1,14 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-import { PrismaClient } from '@prisma/client/';
+const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
 
-export const register  = async  (req, res) => {
+const register  = async  (req, res) => {
     const  {email, password} = req.body;
     try  {
-        const  hashedPassword  = await bcrypt.hash(password, 10);
-        const  user  = await prisma.user.create({
+        const hashedPassword  = await bcrypt.hash(password, 10);
+        const user  = await prisma.user.create({
             data: {
                 email,
                 password: hashedPassword,
@@ -20,3 +20,30 @@ export const register  = async  (req, res) => {
     }
 
 }
+
+const login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await prisma.user.findUnique({
+            where: { email },
+        });
+        if (!user) {
+            return res.status(401).json({ message: 'Invalid credentials' });
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: 'Invalid password' });
+        }
+        const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET, {
+            expiresIn: process.env.JWT_EXPIRATION,
+        });
+        res.status(200).json({ message: 'Login successful', token });
+    } catch (error) {
+        res.status(500).json({ message: 'Error logging in', error });
+    }
+}
+
+module.exports = {
+    register,
+    login,
+};
